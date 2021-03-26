a873114796ab6a809d3d5b9edd825b7617afeb56
lisswart <arnoudlisa@gmail.com>
2021-03-26 18:26:59 -0400

    fixed layout using flexbox

---
+++ index.js
@@ -0,0 +1,9 @@
+// const h2 = document.createElement('h2');
+// document.getElementById('header').appendChild(h2);
+// h2.innerHTML = 'subheader';
+
+// const card4 = document.createElement('div');
+// document.getElementById('card-container').appendChild(card4);
+// card4.innerHTML = 'card 4';
+
+
