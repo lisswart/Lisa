6d1165314db8d2acea1e10e20abca0c4fae4c99e
lisswart <arnoudlisa@gmail.com>
2021-03-27 19:38:12 -0400

    modified DOM using JS

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-// const h2 = document.createElement('h2');
-// document.getElementById('header').appendChild(h2);
-// h2.innerHTML = 'subheader';
+const recipes = [
+{name: "Pork Fry with Chili", ingredientList: "", instructions: ""},
+{name: "Red Beans and Pork", ingredientList: "", instructions: ""},
+{name: "Bird in a Thick Sauce", ingredientList: "", instructions: ""},
+{name: "Chili-Hot Chicken", ingredientList: "", instructions: ""},
+{name: "Roast Duck with Peanut Spice Paste", ingredientList: "", instructions: ""},
+{name: "Duck Curry in Nut Sauce", ingredientList: "", instructions: ""}
+];
 
-// const card4 = document.createElement('div');
-// document.getElementById('card-container').appendChild(card4);
-// card4.innerHTML = 'card 4';
+let tiles = document.getElementsByClassName("tiles");
 
+for(let i = 0; i < tiles.length; i++) {
+    let h2 = tiles[i].getElementsByTagName('h2')[0];
+    let p1 = tiles[i].getElementsByTagName('p')[0];
+    let p2 = tiles[i].getElementsByTagName('p')[1];
+
+    h2.innerText = recipes[i].name;
+    p1.innerText = "ingredients: " + recipes[i].ingredientList;
+    p2.innerText = `instructions: ${recipes[i].instructions}`;
+
+}
 
