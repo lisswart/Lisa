7c9817ea73992a77635e254b27e56d375a700cba
lisswart <arnoudlisa@gmail.com>
2021-03-27 21:46:51 -0400

    removed tiles inside section tag; used a JS function to create tiles

--- index.js
+++ index.js
@@ -7,16 +7,30 @@ const recipes = [
 {name: "Duck Curry in Nut Sauce", ingredientList: "", instructions: ""}
 ];
 
-let tiles = document.getElementsByClassName("tiles");
 
-for(let i = 0; i < tiles.length; i++) {
-    let h2 = tiles[i].getElementsByTagName('h2')[0];
-    let p1 = tiles[i].getElementsByTagName('p')[0];
-    let p2 = tiles[i].getElementsByTagName('p')[1];
+function createTileFromRecipe (recipe) {
+    const tile = document.createElement('div');
+    // const innerTile = document.createElement('div');
 
-    h2.innerText = recipes[i].name;
-    p1.innerText = "ingredients: " + recipes[i].ingredientList;
-    p2.innerText = `instructions: ${recipes[i].instructions}`;
+    tile.className = 'tile';
+    // innerTile.className = 'innerTile';
+    tile.innerHTML =
+    `
+        <div>
+            <h2>${recipe.name}</h2>
+            <div>
+                <p>INGREDIENTS: ${recipe.ingredientList}</p>
+                <p>INSTRUCTIONS: ${recipe.instructions}</p>
+            </div>
+        </div>
+    `;
 
+    return tile;
 }
 
+const tileContainer = document.getElementsByClassName('container')[0];
+
+for(let i = 0; i < recipes.length; i++) {
+    const tile = createTileFromRecipe(recipes[i]);
+    tileContainer.appendChild(tile);
+}
